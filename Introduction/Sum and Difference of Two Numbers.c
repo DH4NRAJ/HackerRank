4becebbb12bfd7381c024546db2fd112d7d275a6
DH4NRAJ <56040211+DH4NRAJ@users.noreply.github.com>
2019-10-03 19:23:12 +0530

    Add files via upload

---
+++ Introduction/Sum and Difference of Two Numbers.c
@@ -0,0 +1,14 @@
+//Sum and Difference of Two Numbers
+#include<stdio.h>
+#include<stdlib.h>
+#include<string.h>
+#include<math.h>
+int main()
+{
+    int a,b;
+    float c,d;
+    scanf("%d%d",&a,&b);
+    scanf("%f%f",&c,&d);
+    printf("%d",a+b);printf(" ");printf("%d\n",a-b);
+    printf("%.1f",c+d);printf(" ");printf("%.1f",c-d);
+}
