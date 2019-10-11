a226d4fa439f6ef520b04a29d977d6f79b6714f1
DH4NRAJ <56040211+DH4NRAJ@users.noreply.github.com>
2019-10-11 23:36:10 +0530

    Create Solution.java

---
+++ Java/Introduction/Java Loops II/Solution.java
@@ -0,0 +1,26 @@
+//Java Loops II
+import java.util.*;
+import java.io.*;
+
+class Solution{
+    public static void main(String []argh){
+        Scanner in = new Scanner(System.in);
+        int t=in.nextInt();
+        for(int i=0;i<t;i++){
+            int a = in.nextInt();
+            int b = in.nextInt();
+            int n = in.nextInt();
+            int s = a+b;
+            System.out.print(s+" ");
+            for(int j=1;j<n;j++)
+            {
+                s += (Math.pow(2,j)*b);
+                System.out.print(s+" ");
+            }
+            System.out.println("");
+        }
+        in.close();
+    }
+}
+
+
