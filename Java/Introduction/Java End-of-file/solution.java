d9c232a15936c79cb939ff713651ac1adc193e52
DH4NRAJ <56040211+DH4NRAJ@users.noreply.github.com>
2019-11-06 23:26:57 +0530

    Create solution.java

---
+++ Java/Introduction/Java End-of-file/solution.java
@@ -0,0 +1,19 @@
+import java.io.*;
+import java.util.*;
+import java.text.*;
+import java.math.*;
+import java.util.regex.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+
+    Scanner s = new Scanner(System.in);
+    int count=0;
+    while (s.hasNext()) {
+      count++;
+      System.out.println(count+" "+s.nextLine());
+    }
+    s.close();
+    }
+}
