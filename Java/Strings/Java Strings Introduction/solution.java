98dadbf0eae3755105646f0cc4ca09b1442767af
DH4NRAJ <56040211+DH4NRAJ@users.noreply.github.com>
2019-11-14 17:24:07 +0530

    Create solution.java

---
+++ Java/Strings/Java Strings Introduction/solution.java
@@ -0,0 +1,24 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        String B=sc.next();
+        sc.close();
+        System.out.println(A.length() + B.length());
+        System.out.println(A.compareTo(B) > 0 ? "Yes" : "No");
+        System.out.println(capFirstLetter(A) + " " + capFirstLetter(B));
+    }
+    
+    private static String capFirstLetter(String str) {
+        if (str == null || str.length() == 0) {
+            return "";
+        } else {
+            return str.substring(0,1).toUpperCase() + str.substring(1);
+        }
+    }
+}
