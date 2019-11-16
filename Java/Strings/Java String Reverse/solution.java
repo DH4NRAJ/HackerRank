dc7adc186accedda3f7c486e47b4e4722858844e
DH4NRAJ <56040211+DH4NRAJ@users.noreply.github.com>
2019-11-16 14:21:52 +0530

    Create solution.java

---
+++ Java/Strings/Java String Reverse/solution.java
@@ -0,0 +1,14 @@
+import java.io.*;
+import java.util.*;
+
+public class Solution {
+
+    public static void main(String[] args) {
+        
+        Scanner sc=new Scanner(System.in);
+        String A=sc.next();
+        sc.close();
+        String rev = new StringBuilder(A).reverse().toString();
+        System.out.println(A.equals(rev)?"Yes":"No");
+    }
+}
