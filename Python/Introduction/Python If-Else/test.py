5197a1f27aeb46601b19a883ea0015689909c6a9
Dhanraj K <56040211+DH4NRAJ@users.noreply.github.com>
2020-01-01 09:38:46 +0530

    Update test.py

--- Python/Introduction/Python If-Else/test.py
+++ Python/Introduction/Python If-Else/test.py
@@ -14,9 +14,24 @@ assert(n<=100 and n>=1)
 if (n%2 == 0):
     if (n>2 and n<5):
         print('Not Weird')
-    elif (n>6 and n<20):
+    elif (n>=6 and n<=20):
         print('Weird')
     else :
         print('Not Weird')
 else :
     print('Weird')
+    
+    
+    
+    
+    
+'''
+check = {True: "Not Weird", False: "Weird"}
+
+print(check[
+        n%2==0 and (
+            n in range(2,6) or 
+            n > 20)
+    ])
+#alternative     
+'''
