6a9b1a94f847803ed65aad53472e7626da3e6a83
Dhanraj K <56040211+DH4NRAJ@users.noreply.github.com>
2020-01-01 10:34:21 +0530

    Update test.py

--- Python/Introduction/Write a function/test.py
+++ Python/Introduction/Write a function/test.py
@@ -1,13 +1,4 @@
 def is_leap(year):
-    leap = False
-    if (year%4==0 and year%100==0):
-        if(year%400 == 0):
-            leap = True
-        else:
-            leap = False
-    else :
-        leap = False
-    return leap
-
+    return year % 4 == 0 and (year % 400 == 0 or year % 100 != 0)
 year = int(input())
 print(is_leap(year))
