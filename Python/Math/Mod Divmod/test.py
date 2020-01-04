81ccf66bb212fdfc824c0a5971d6a90528ba72c3
Dhanraj K <56040211+DH4NRAJ@users.noreply.github.com>
2020-01-04 16:09:46 +0530

    Update test.py

--- Python/Math/Mod Divmod/test.py
+++ Python/Math/Mod Divmod/test.py
@@ -2,3 +2,8 @@ import math
 a = int(input())
 b = int(input())
 print (divmod(a,b)[0],divmod(a,b)[1],divmod(a,b),sep="\n")
+
+'''
+#Effective Way
+print('{0}\n{1}\n({0}, {1})'.format(*divmod(int(input()), int(input()))))
+'''
