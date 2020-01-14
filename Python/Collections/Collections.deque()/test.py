b534508f43b8361e820644e574c39d5f0ea832c2
Dhanraj K <56040211+DH4NRAJ@users.noreply.github.com>
2020-01-14 13:35:29 +0530

    Create test.py

---
+++ Python/Collections/Collections.deque()/test.py
@@ -0,0 +1,15 @@
+from collections import deque
+d = deque()
+for _ in range(int(input())):
+    l = input().split()
+    if l[0] == 'append':
+        d.append(l[1])
+    elif l[0] == 'appendleft':
+        d.appendleft(l[1])
+    elif l[0] == 'pop':
+        d.pop()
+    elif l[0] == 'popleft':
+        d.popleft()
+    else :
+        pass
+print(' '.join(d))
