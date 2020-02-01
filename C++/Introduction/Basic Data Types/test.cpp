6c5b202dd4b426bf1bfb630b277fe9d7a9ef685d
Dhanraj K <56040211+DH4NRAJ@users.noreply.github.com>
2020-02-01 18:34:30 +0530

    Create test.cpp

---
+++ C++/Introduction/Basic Data Types/test.cpp
@@ -0,0 +1,18 @@
+#include <iostream>
+#include <cstdio>
+using namespace std;
+
+int main() {
+    int a;
+    long b;
+    char c;
+    float d;
+    double e;
+    cin>>a>>b>>c>>d>>e;
+    printf("%d\n%ld\n%c\n%.3f\n%.6lf",a,b,c,d,e);
+    return 0;
+    
+}
+/*
+cout.precision(3);//can also be used
+*/
