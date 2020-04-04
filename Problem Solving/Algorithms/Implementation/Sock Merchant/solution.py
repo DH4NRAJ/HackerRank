9ed03562ffb1f7a6cd650b5630f491faa2ba20a5
Dhanraj K <56040211+DH4NRAJ@users.noreply.github.com>
2020-04-04 19:11:51 +0530

    Create solution.py

---
+++ Problem Solving/Algorithms/Implementation/Sock Merchant/solution.py
@@ -0,0 +1,28 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+# Complete the sockMerchant function below.
+def sockMerchant(n, ar):
+    count = 0
+    ar_set = set(ar)
+    for i in ar_set:
+        count += ar.count(i) % 2
+    return (n-count)//2
+
+if __name__ == '__main__':
+    fptr = open(os.environ['OUTPUT_PATH'], 'w')
+
+    n = int(input())
+
+    ar = list(map(int, input().rstrip().split()))
+
+    result = sockMerchant(n, ar)
+
+    fptr.write(str(result) + '\n')
+
+    fptr.close()
